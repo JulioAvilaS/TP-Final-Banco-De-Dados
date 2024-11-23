beeb3b6c000a2a922c86ea58d52a0b0e76d25b49
Julio Avila <julius7sc@gmail.com>
2024-11-22 21:02:19 -0300

    update models

--- obj/Debug/net8.0/AplicationTpDB.AssemblyInfo.cs
+++ obj/Debug/net8.0/AplicationTpDB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AplicationTpDB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48aeca7addebc82c7f2889d6f07827b207f28f51")]
 [assembly: System.Reflection.AssemblyProductAttribute("AplicationTpDB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AplicationTpDB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
